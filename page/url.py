bbda7fd258523f1da016e80897b22b5e6219f2bc
YuMinHa <qhahd78>
2020-08-01 19:50:57 +0900

    block

---
+++ page/url.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('page.urls')),
+    pathpath('profile/<int:designer_id>', views.detail, name = "detail"),
+]
